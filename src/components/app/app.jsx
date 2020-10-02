976912c45e791c2fe757d48e7dc36418904277b3
Elizaveta Ozerova <e.ozerova@adsterra.com>
2020-10-02 10:52:34 +0300

    module2-task1 настроена маршрутизация, исправлены пути до bundle.js и main.min.css

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
+import AddReview from '../add-review/add-review';
+import Auth from '../auth/auth';
 import MainPage from '../main-page/main-page';
+import MoviePage from '../movie-page/movie-page';
+import Player from '../player/player';
+import UserCatalog from '../user-catalog/user-catalog';
 
 const App = (props) => {
   const {mainMovie} = props;
 
   return (
-    <MainPage mainMovie={mainMovie} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/'>
+          <MainPage mainMovie={mainMovie} />
+        </Route>
+        <Route exact path='/login'>
+          <Auth />
+        </Route>
+        <Route exact path='/mylist'>
+          <UserCatalog />
+        </Route>
+        <Route exact path='/films/:id'>
+          <MoviePage />
+        </Route>
+        <Route exact path='/films/:id/review'>
+          <AddReview />
+        </Route>
+        <Route exact path='/player/:id'>
+          <Player />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
